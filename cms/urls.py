21ba251c9e032e52867cfa9c979fd1133237be23
Nando-bog <fernando.diazdelcastillo@gmail.com>
2013-12-28 23:22:40 -0500

    Fixed urls

--- cms/urls.py
+++ cms/urls.py
@@ -1,4 +1,6 @@
 #coding=utf-8
+#CMS project URLS
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.dates import DateDetailView, YearArchiveView, MonthArchiveView, DayArchiveView
@@ -7,11 +9,6 @@ from django.conf.urls.static import static
 from coltrane.models import Entry
 
 admin.autodiscover()
-
-entry_info_dict = {
-    'queryset': Entry.objects.all(),
-    'date_field': 'pub_date',
-}
 
 urlpatterns = patterns('',
     # Examples:
@@ -26,10 +23,5 @@ urlpatterns = patterns('',
     url(r'^home', 'search.views.home'),
     url(r'^index', 'search.views.home'),
     (r'^$', 'search.views.home'),
-    url(r'^weblog/$', 'coltrane.views.entries_index'),    #(r'^weblog/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/{0,1}$', 'coltrane.views.entry_detail'),
-    #(r'', include('django.contrib.flatpages.urls')),
-    url(r'^weblog/(?P<year>\d{4})/{0,1}$', YearArchiveView.as_view(model=Entry, date_field="pub_date", make_object_list = True)),
-    url(r'^weblog/(?P<year>\d{4})/(?P<month>\d{2})/{0,1}$', MonthArchiveView.as_view(model=Entry, date_field="pub_date", month_format="%m")),
-    url(r'^weblog/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/{0,1}$', DayArchiveView.as_view(model=Entry, date_field="pub_date", month_format="%m", allow_future=True)),
-    url(r'^weblog/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/{0,1}$', DateDetailView.as_view(model=Entry, date_field="pub_date", month_format="%m", allow_future=True)),
+    url(r'^weblog/', include('coltrane.urls')),
 )
